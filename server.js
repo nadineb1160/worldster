2576445c6b6a2fa369cb0af77a9f5ee681214aa8
nadineb <nadineb@umich.edu>
2020-04-22 21:27:51 -0700

    [add] PORT, handlebars, routes

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/worldster_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening at localhost:" + PORT);
+})
