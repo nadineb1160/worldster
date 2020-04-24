a0f75e7efff80c7af2a01a1115bdf1d4b7537c4a
nadineb <nadineb@umich.edu>
2020-04-23 17:58:07 -0700

    [change] table name to worldster

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ DROP DATABASE IF EXISTS worldster_db;
 CREATE DATABASE worldster_db;
 USE worldster_db;
 
-CREATE TABLE worldster_db (
+CREATE TABLE worldster (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    country_name VARCHAR(30) NOT NULL;
-    visited BOOLEAN DEFAULT false;
+    country_name VARCHAR(30) NOT NULL,
+    visited BOOLEAN DEFAULT false
 );
