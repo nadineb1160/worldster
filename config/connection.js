7d5532ed2ecd24c8b35f6a88a377cb6a8e2bb0f7
nadineb <nadineb@umich.edu>
2020-04-23 17:53:56 -0700

    [change] db name

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "password",
-    database: "worldster",
+    database: "worldster_db",
 });
 
 connection.connect(function(err) {
