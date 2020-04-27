6bfe0650a87ece1b24f751745d645bed2231ebf6
nadineb <nadineb@umich.edu>
2020-04-27 09:54:40 -0700

    [change] connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "worldster_db",
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect(function(err) {
     if (err) {
